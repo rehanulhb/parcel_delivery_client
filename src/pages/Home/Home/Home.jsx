547b6c8b2d7438922bcae5de87e8ccac93bbeb8c
rehanulhb <rehanulh@gmail.com>
2026-06-20 21:42:06 +0200

    Coverage Added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,15 @@
+import Reviews from "../../Reviews/Reviews";
 import Banner from "../Banner/Banner";
 import Brands from "../Brands/Brands";
+
+const reviewsPromise = fetch("/reviews.json").then((res) => res.json());
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <Brands></Brands>
+      <Reviews reviewsPromise={reviewsPromise}></Reviews>
     </div>
   );
 };
